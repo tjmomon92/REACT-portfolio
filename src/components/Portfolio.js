bec915b1ac9a787004e2c3b8f228d2ebd143ef34
TJ Momon <tjmomon92@gmail.com>
2023-04-27 12:09:25 -0500

    created individual component to be used for each project instance

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,108 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
 export const Portfolio = () => {
+    const projects = [
+        {
+            title: "placeholder 1",
+            description: "placeholder 1",
+            imgUrl: projImg1,
+        },
+        {
+            title: "placeholder 2",
+            description: "placeholder 2",
+            imgUrl: projImg2,
+        },
+        {
+            title: "placeholder 3",
+            description: "placehelder 3",
+            imgUrl: projImg3,
+        },
+        {
+            title: "placeholder 4",
+            description: "placeholder 4",
+            imgUrl: projImg4,
+        },
+        {
+            title: "placeholder 5",
+            description: "placeholder 5",
+            imgUrl: projImg5,
+        },
+        {
+            title: "placeholder 6",
+            description: "placeholder 6",
+            imgUrl: projImg6,
+        },
+    ];
+
     return (
-        
+        <section className="portfolio" id="portfolio">
+            <Container>
+                <Row>
+                    <Col>
+                    <h2>My Portfolio</h2>
+                    <p>Lorem Ipsum</p>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     )
+}
+
+return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indus
+                {/* <Tab.Container id="projecttry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>s-tabs" defaultActiveKey="first"> */}
+                  {/* <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav> */}
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
 }
