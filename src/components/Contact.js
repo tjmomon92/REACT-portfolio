1ceb07da7cd7790de91fe551fbeb56ecfa4928bc
TJ Momon <tjmomon92@gmail.com>
2023-04-29 13:42:33 -0500

    beginning work on contactMe email functionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -19,6 +19,9 @@ export const Contact = () => {
             [category]: value
         })
     }
+    const handleSubmit = () => {
+
+    }
 
     return (
         <section className='contact' id='connect'>
@@ -26,7 +29,7 @@ export const Contact = () => {
                 <Row className='align-items-center'>
                     <Col md={6}>
                         <h2>Get in Touch</h2>
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <Row>
                                 <Col sm={6} className='px-1'>
                                     <input type='text' value={formDetails.firstName} placeholder='First Name' onChange={(e) => onFormUpdate('firstName', e.target.value)} />
@@ -34,6 +37,22 @@ export const Contact = () => {
                                 <Col sm={6} className='px-1'>
                                     <input type='text' value={formDetails.lastName} placeholder='Last Name' onChange={(e) => onFormUpdate('lastName', e.target.value)} />
                                 </Col>
+                                <Col sm={6} className='px-1'>
+                                    <input type='email' value={formDetails.email} placeholder='Email Address' onChange={(e) => onFormUpdate('email', e.target.value)} />
+                                </Col>
+                                <Col sm={6} className='px-1'>
+                                    <input type='tel' value={formDetails.phone} placeholder='Phone Number' onChange={(e) => onFormUpdate('phone', e.target.value)} />
+                                </Col>
+                                <Col>
+                                    <textarea row='6' value={formDetails.message} placeholder='Message' onChange={(e) => onFormUpdate('message', e.target.value)} />
+                                    <button type='submit'><span>{buttonText}</span></button>
+                                </Col>
+                                {
+                                    status.message &&
+                                    <Col>
+                                        <p className={status.sucess === false ? 'danger' : 'success'}>{status.message}</p>
+                                    </Col>
+                                }
                             </Row>
                         </form>
                     </Col>
