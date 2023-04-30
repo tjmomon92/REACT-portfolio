40a0191caff4a9339f6f7ba0d75001f6cd62427f
TJ Momon <tjmomon92@gmail.com>
2023-04-30 18:10:01 -0500

    Nav link troubleshooting

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,13 +21,13 @@ export const NavBar = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <div onClick={() => window.location.replace("REACT-portfolio/#home")}>
-                            <Nav.Link href="#about" className={ activeLink === 'about' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('about')}>About Me</Nav.Link>
+                            <Nav.Link href="#about" className='navbar-link'>About Me</Nav.Link>
                         </div>
                         <div onClick={() => window.location.replace("REACT-portfolio/#portfolio")}>
-                            <Nav.Link href="#portfolio" className={ activeLink === 'portfolio' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('portfolio')}>Portfolio</Nav.Link>
+                            <Nav.Link href="#portfolio" className='navbar-link'>Portfolio</Nav.Link>
                         </div>
                         <div onClick={() => window.location.replace("REACT-portfolio/#resume")}>
-                            <Nav.Link href="#resume" className={ activeLink === 'resume' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('resume')}>My Resume</Nav.Link>
+                            <Nav.Link href="#resume" className='navbar-link'>My Resume</Nav.Link>
                         </div>
                     </Nav>
                     <span className="contact-me">
