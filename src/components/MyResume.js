a742700bb720165ebc8b654e470cb19611fca1f1
TJ Momon <tjmomon92@gmail.com>
2023-04-30 11:13:26 -0500

    added resume download functionality

--- src/components/MyResume.js
+++ src/components/MyResume.js
@@ -2,6 +2,7 @@ import { Container, Col, Row } from "react-bootstrap";
 // import myAvatar from "../Assets/img/avatar.png"
 // import { useEffect, useState } from "react";
 import { MyCube } from "./Cube.js";
+import resume from "../Assets/img/Resume.pdf";
 
 export const MyResume = () => {
     return(
@@ -16,7 +17,7 @@ export const MyResume = () => {
                     </Col>
                     <Col className="right-resume" xs={12} md={6} xl={7}>
                         <h1 className="resume-title">What I've done</h1>
-                        <p>Technology has always fascinated me, and I have always had a passion for figuring out how things work. With this driving force pushing me, I enrolled into a full-stack development bootcamp with SMU to follow this calling. I am very proud to say that I have learned HTML, CSS, JavaScript, Node.js, and even SQL and my new favorite language REACT. For more information, check out my resume here.</p>
+                        <p>Technology has always fascinated me, and I have always had a passion for figuring out how things work. With this driving force pushing me, I enrolled into a full-stack development bootcamp with SMU to follow this calling. I am very proud to say that I have learned HTML, CSS, JavaScript, Node.js, and even SQL and my new favorite language REACT. For more information, check out my resume <a href={resume}>here.</a></p>
                     </Col>
                 </Row>
             </Container>
